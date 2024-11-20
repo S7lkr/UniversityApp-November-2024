2efd9e8146f8f5cd537fe0b03274618e253068e9
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-20 22:22:34 +0200

    Fix: update settings.py -> AUTH_USER_MODEL

--- UniversityApp/settings.py
+++ UniversityApp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'UniversityApp.accounts.User'
+AUTH_USER_MODEL = 'accounts.CustomUser'
