c128173c2185e00e23f0398984cf28335c84d421
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-20 12:49:37 +0200

    Create: user's profile model

---
+++ UniversityApp/accounts/models/profile.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+UserModel = get_user_model()
+
+
+class Profile(models.Model):
+    first_name = models.CharField(
+        max_length=15,
+        validators=[
+            MinLengthValidator(2),
+        ]
+    )
+    last_name = models.CharField(
+        max_length=20,
+        validators=[
+            MinLengthValidator(2),
+        ]
+    )
+    age = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+    )
+    personal_image = models.URLField(
+        null=True,
+        blank=True,
+    )
+    user = models.OneToOneField(
+        to=UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
