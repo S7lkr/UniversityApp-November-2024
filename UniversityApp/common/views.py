a1e04183b081a4e7156ca808bfa68f83c0213d19
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-21 10:01:32 +0200

    Create: Home page

--- UniversityApp/common/views.py
+++ UniversityApp/common/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomePage(generic.ListView):
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        pass
