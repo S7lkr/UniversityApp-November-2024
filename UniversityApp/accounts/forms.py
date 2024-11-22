43e22ddd9ccd9e26091148d4f2ede1c6a30798c9
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-22 23:42:11 +0200

    Create forms for accounts app

---
+++ UniversityApp/accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.contrib.auth import get_user_model
+from UniversityApp.accounts.models import Profile
+
+User = get_user_model()     # get User model
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+
+class UserEditForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = "__all__"
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ("user",)
