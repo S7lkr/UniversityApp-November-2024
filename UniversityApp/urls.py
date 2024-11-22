3f98658b45892cabf847045b1fb2a8864e179e91
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-22 21:05:42 +0200

    Add: account urls to project urls

--- UniversityApp/urls.py
+++ UniversityApp/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('UniversityApp.accounts.urls')),
     path('', include('UniversityApp.common.urls')),
 ]
