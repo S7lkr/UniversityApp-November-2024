47a6c3f291b9d845e4dc27cc124bafa03a307939
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-22 23:55:32 +0200

    Register user model in admin.py

--- UniversityApp/accounts/admin.py
+++ UniversityApp/accounts/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm
 
 UserModel = get_user_model()
 
 
 @admin.register(UserModel)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdmin):
+    model = UserModel
+    add_form = UserRegisterForm
+    form = UserEditForm
+    list_display = ('email', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('pk',)
+
+    # categories in admin panel (user information):
+    fieldsets = (
+        ('User email/password', {'fields': ('email', 'password')}),
+        ('User info', {'fields': ()}),
+        ('User Permissions', {'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')}),
+        ('User\'s last login', {'fields': ('last_login',)})
+    )
+
+    # when creating new user, which fields to be shown:
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            }
+        ),
+    )
