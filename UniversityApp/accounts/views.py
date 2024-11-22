346944949263cc4d2f3a887f8886a5c7cdba44df
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-22 23:41:56 +0200

    Create views for accounts app

--- UniversityApp/accounts/views.py
+++ UniversityApp/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm
+from django.contrib.auth.views import LoginView
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class UserRegisterPage(generic.CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('home')
+
+
+class UserLoginPage(LoginView):
+    template_name = 'users/login.html'
+
+
+class UserEditPage(generic.UpdateView):
+    form_class = UserEditForm
+    template_name = 'index.html'
+    success_url = reverse_lazy('home')
+
+
+class ProfileDetailsPage(generic.DetailView):
+    pass
+
+
+class ProfileDeletePage(generic.DeleteView):
+    pass
