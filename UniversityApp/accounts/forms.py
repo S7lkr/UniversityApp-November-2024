a3e9c4f4a7be64eb9ad532b8e83396e08feb12fb
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-23 03:42:59 +0200

    Fix: placeholder mixin

--- UniversityApp/accounts/forms.py
+++ UniversityApp/accounts/forms.py
@@ -2,11 +2,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from django.contrib.auth import get_user_model
 from UniversityApp.accounts.models import Profile
+from UniversityApp.mixins import PlaceholderMixin
 
 User = get_user_model()     # get User model
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(PlaceholderMixin, UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('email', 'password1', 'password2')
--- UniversityApp/mixins/mixins.py
+++ UniversityApp/mixins/mixins.py
@@ -2,7 +2,7 @@ class PlaceholderMixin:
     def attach_placeholders(self):
         for field_name, field in self.fields.items():
             placeholder = field.label or field_name.replace('_', ' ').capitalize()
-            field.widget.attrs['placeholder'] = placeholder
+            field.widget.attrs['placeholder'] = placeholder + "..."
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
