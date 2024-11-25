74c701eaceb62aa95478449e224d1990440f7670
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-25 02:49:20 +0200

    Create: course app views

--- UniversityApp/courses/views.py
+++ UniversityApp/courses/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import generic
+from UniversityApp.courses.models import Course
 
-# Create your views here.
+
+class CoursesCategoriesPage(generic.TemplateView):
+    template_name = 'courses_categories.html'
+
+
+class CoursesPage(generic.ListView):
+    model = Course
+    template_name = 'courses.html'
+    slug_url_kwarg = 'category'
+    context_object_name = 'courses'
+
+    def get_queryset(self):
+        # get 'course category' dynamically (from url)
+        category_url = self.request.get_full_path().split('/')[3].replace("-", " ")
+        queryset = super().get_queryset().filter(category=category_url)
+        return queryset
