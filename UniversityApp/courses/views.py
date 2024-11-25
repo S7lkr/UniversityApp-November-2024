b1bab61f9ca060e3a581465e4adcc51aa50956c2
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-25 21:37:09 +0200

    Fix: template_name in courses view

--- UniversityApp/courses/views.py
+++ UniversityApp/courses/views.py
@@ -1,17 +1,16 @@
 from django.urls import reverse_lazy
 from django.views import generic
-
 from UniversityApp.courses.forms import CourseForm
 from UniversityApp.courses.models import Course
 
 
 class CoursesCategoriesPage(generic.TemplateView):
-    template_name = 'courses_categories.html'
+    template_name = 'courses/courses_categories.html'
 
 
 class CoursesPage(generic.ListView):
     model = Course
-    template_name = 'courses.html'
+    template_name = 'courses/courses.html'
     slug_url_kwarg = 'category'
     context_object_name = 'courses'
 
@@ -22,8 +21,16 @@ class CoursesPage(generic.ListView):
         return queryset
 
 
-class CourseCreate(generic.CreateView):
+class CourseCreatePage(generic.CreateView):
     model = Course
     form_class = CourseForm
-    template_name = 'course-create-page.html'
+    template_name = 'courses/course-create-page.html'
+    success_url = reverse_lazy('courses-wd')
+
+
+class CourseEditPage(generic.UpdateView):
+    model = Course
+    form_class = CourseForm
+    pk_url_kwarg = 'pk'
+    template_name = 'courses/course-edit-page.html'
     success_url = reverse_lazy('courses-wd')
