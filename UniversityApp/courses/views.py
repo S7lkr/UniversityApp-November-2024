12fd08da48a8ce611b4043fee30b3261a2f933ea
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 12:04:32 +0200

    Update: acquire object (edit/del) by slug

--- UniversityApp/courses/views.py
+++ UniversityApp/courses/views.py
@@ -9,7 +9,18 @@ class CoursesCategoriesPage(generic.TemplateView):
     template_name = 'courses/courses_categories.html'
 
 
-class CoursesPage(generic.ListView):
+class CoursesAllPage(generic.ListView):
+    model = Course
+    template_name = 'courses/courses.html'
+    context_object_name = 'courses'
+
+    def get_queryset(self):
+        all_courses = super().get_queryset().all()
+        print(all_courses)
+        return all_courses
+
+
+class CoursesFromCategoryPage(generic.ListView):
     model = Course
     template_name = 'courses/courses.html'
     slug_url_kwarg = 'category'
@@ -20,6 +31,12 @@ class CoursesPage(generic.ListView):
         category_url = self.request.get_full_path().split('/')[3].replace("-", " ")
         queryset = super().get_queryset().filter(category=category_url)
         return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if context['courses']:
+            context['category'] = self.object_list.first().category
+        return context
 
 
 class CourseCreatePage(generic.CreateView):
@@ -32,23 +49,28 @@ class CourseCreatePage(generic.CreateView):
 class CourseEditPage(generic.UpdateView):
     model = Course
     form_class = forms.CourseEditForm
-    pk_url_kwarg = 'pk'
+    slug_url_kwarg = 'course_slug'
     template_name = 'courses/course-edit-page.html'
-    success_url = reverse_lazy('courses-wd')
+    # success_url = reverse_lazy('course-details')
+
+    def get_success_url(self):
+        course_slug = self.object.slug
+        print(course_slug)
+        return reverse_lazy('course-details', kwargs={'course_slug': course_slug})
 
 
 class CourseDetailsPage(generic.DetailView):
     model = Course
-    pk_url_kwarg = 'pk'
+    slug_url_kwarg = 'course_slug'
     template_name = 'courses/course-details-page.html'
 
 
 class CourseDeletePage(generic.DeleteView):
     model = Course
     form_class = forms.CourseDeleteForm
-    pk_url_kwarg = 'pk'
+    slug_url_kwarg = 'course_slug'
     template_name = 'courses/course-delete-page.html'
-    success_url = reverse_lazy('courses-wd')
+    success_url = reverse_lazy('courses')
 
     def get_initial(self):
         return self.get_object().__dict__
