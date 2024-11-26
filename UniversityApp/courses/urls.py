4f0b4e05df0390d0c3a65b823a904a41982bd71e
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 12:03:44 +0200

    Edit: reorganize courses urls.py

--- UniversityApp/courses/urls.py
+++ UniversityApp/courses/urls.py
@@ -2,19 +2,44 @@ from django.urls import path, include
 from UniversityApp.courses import views
 
 urlpatterns = [
+    path('all-courses/', views.CoursesAllPage.as_view(), name='courses-all'),
     path('categories/', include([
         path('', views.CoursesCategoriesPage.as_view(), name='categories'),
         path('Web-Design/', include([
-            path('', views.CoursesPage.as_view(), name='courses-wd'),
-            path('create/', views.CourseCreatePage.as_view(), name='courses-wd-create'),
-            path('<int:pk>/', include([
-                path('details/', views.CourseDetailsPage.as_view(), name='course-wd-details'),
-                path('edit/', views.CourseEditPage.as_view(), name='course-wd-edit'),
-                path('delete/', views.CourseDeletePage.as_view(), name='course-wd-delete'),
-            ]))
+            path('', views.CoursesFromCategoryPage.as_view(), name='courses-wd'),
+            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
+            path('<slug:course_slug>/', include([
+                path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
+                path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
+                path('delete/', views.CourseDeletePage.as_view(), name='course-delete'),
+            ])),
         ])),
-        path('Graphic-Design/', views.CoursesPage.as_view(), name='courses-gd'),
-        path('Video-Editing/', views.CoursesPage.as_view(), name='courses-ve'),
-        path('Online-Marketing/', views.CoursesPage.as_view(), name='courses-om'),
+        path('Graphic-Design/', include([
+            path('', views.CoursesFromCategoryPage.as_view(), name='courses-gd'),
+            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
+            path('<slug:course_slug>/', include([
+                path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
+                path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
+                path('delete/', views.CourseDeletePage.as_view(), name='course-delete'),
+            ])),
+        ])),
+        path('Video-Editing/', include([
+            path('', views.CoursesFromCategoryPage.as_view(), name='courses-ve'),
+            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
+            path('<slug:course_slug>/', include([
+                path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
+                path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
+                path('delete/', views.CourseDeletePage.as_view(), name='course-delete'),
+            ])),
+        ])),
+        path('Online-Marketing/', include([
+            path('', views.CoursesFromCategoryPage.as_view(), name='courses-om'),
+            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
+            path('<slug:course_slug>/', include([
+                path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
+                path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
+                path('delete/', views.CourseDeletePage.as_view(), name='course-delete'),
+            ])),
+        ])),
     ])),
 ]
