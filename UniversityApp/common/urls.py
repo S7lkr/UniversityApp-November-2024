5647f38f255c491ab593779d924f21059e27bf3f
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 22:53:57 +0200

    Feat: add user to selected course

--- UniversityApp/common/urls.py
+++ UniversityApp/common/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from UniversityApp.common.views import HomePage
+from UniversityApp.common.views import HomePage, add_user_to_course
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
+    path('add-user-to-course/<slug:course_slug>/', add_user_to_course, name="add-user-to-course"),
 ]
--- UniversityApp/common/views.py
+++ UniversityApp/common/views.py
@@ -1,5 +1,24 @@
+from django.shortcuts import render
 from django.views import generic
+from UniversityApp.courses.models import Course
 
 
 class HomePage(generic.TemplateView):
     template_name = 'index.html'
+
+
+def add_user_to_course(request, course_slug):
+    course_id = Course.objects.get(slug=course_slug).pk
+    profile = request.user.profile
+    profile.course_id = course_id
+    print(request.user)
+    print(course_slug)
+
+    if course_slug:
+        profile.save()
+
+    context = {
+        'slug_url_kwarg': course_slug,
+    }
+
+    return render(request, 'index.html', context)
