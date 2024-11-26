2394e19445426b62b688c45ef4bf315fd0a581a5
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 16:38:34 +0200

    Feat: add profile edit/delete functionality

--- UniversityApp/accounts/views.py
+++ UniversityApp/accounts/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import get_user_model
-
-from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm
+from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.views import LoginView
 from django.views import generic
 from django.urls import reverse_lazy
-
 from UniversityApp.accounts.models import Profile
 
 
@@ -27,7 +25,21 @@ class ProfileDetailsPage(generic.DetailView):
 
 
 class ProfileEditPage(generic.UpdateView):
-    pass
+    model = Profile
+    form_class = ProfileEditForm
+    template_name = 'profile/profile-edit.html'
+    # success_url = reverse_lazy('profile-details')
+
+    def get_success_url(self):
+        return reverse_lazy('profile-details', kwargs={'pk': self.object.pk})
+
+    # def get_queryset(self):
+    #     queryset = self.get_queryset()
+    #     print(queryset)
+    #     return queryset
+
+    # def get_object(self, queryset=None):
+    #     return self.request.user
 
 
 class ProfileDeletePage(generic.DeleteView):
