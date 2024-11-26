2458b1457dbcc209b999d6d10fa40e7f96011506
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 22:55:16 +0200

    Feat: login user after registration

--- UniversityApp/accounts/views.py
+++ UniversityApp/accounts/views.py
@@ -1,15 +1,25 @@
-from django.contrib.auth import get_user_model
-from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm, ProfileEditForm
+from django.contrib.auth import get_user_model, login
+from UniversityApp.accounts.forms import UserRegisterForm, ProfileEditForm
 from django.contrib.auth.views import LoginView
 from django.views import generic
 from django.urls import reverse_lazy
 from UniversityApp.accounts.models import Profile
 
+User = get_user_model()
+
 
 class UserRegisterPage(generic.CreateView):
+    model = User
     form_class = UserRegisterForm
     template_name = 'registration/registration.html'
-    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('profile-edit', kwargs={'pk': self.object.pk})
 
 
 class UserLoginPage(LoginView):
