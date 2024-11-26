665d8412612e23577fe8e03b2da8cecacb798ded
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 15:28:51 +0200

    Add: profile details functionality

--- UniversityApp/accounts/urls.py
+++ UniversityApp/accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from UniversityApp.accounts import views
 
 urlpatterns = [
-    path('register/', views.UserRegisterPage.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('', include([
+        path('register/', views.UserRegisterPage.as_view(), name='register'),
+        path('login/', views.LoginView.as_view(), name='login'),
+    ])),
+    path('<int:pk>/', include([
+        path('details/', views.ProfileDetailsPage.as_view(), name='profile-details'),
+    ])),
 ]
--- UniversityApp/accounts/views.py
+++ UniversityApp/accounts/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+
 from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm
 from django.contrib.auth.views import LoginView
 from django.views import generic
 from django.urls import reverse_lazy
+
+from UniversityApp.accounts.models import Profile
 
 
 class UserRegisterPage(generic.CreateView):
@@ -15,7 +19,11 @@ class UserLoginPage(LoginView):
 
 
 class ProfileDetailsPage(generic.DetailView):
-    pass
+    model = get_user_model()
+    template_name = 'profile/profile-details.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 class ProfileEditPage(generic.UpdateView):
