fb7f852c3a4d249b040a5bd78b81439467810224
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-26 22:59:55 +0200

    Fix: disable labels in edit/del forms

--- UniversityApp/accounts/forms.py
+++ UniversityApp/accounts/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from django.contrib.auth import get_user_model
 from UniversityApp.accounts.models import Profile
-from UniversityApp.mixins import PlaceholderMixin
+from UniversityApp.mixins import PlaceholderMixin, DisabledFieldsMixin
 
 User = get_user_model()     # get User model
 
@@ -24,13 +24,29 @@ class UserRegisterForm(PlaceholderMixin, UserCreationForm):
         # }
 
 
-class UserEditForm(UserChangeForm):
+class UserEditForm(UserChangeForm):     # for admin
     class Meta(UserChangeForm.Meta):
         model = User
         fields = "__all__"
 
 
-class ProfileEditForm(forms.ModelForm):
+class ProfileEditForm(PlaceholderMixin, forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ("user",)
+        exclude = ("user", "course")
+        labels = {
+            'first_name': '',
+            'last_name': '',
+            'personal_image': '',
+            'biography': '',
+            'age': '',
+            'type': "Select user type",
+        }
+
+
+class ProfileDeleteForm(DisabledFieldsMixin, forms.ModelForm):
+    disabled_fields_set = ('first_name', 'last_name', 'type')
+
+    class Meta:
+        model = Profile
+        fields = ('first_name', 'last_name', 'type')
