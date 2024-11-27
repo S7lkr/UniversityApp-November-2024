f456b3842f415b9d12c13a73e4c372669bb06883
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-27 23:09:44 +0200

    Feat: show profile info in admin panel

--- UniversityApp/accounts/admin.py
+++ UniversityApp/accounts/admin.py
@@ -2,20 +2,36 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from UniversityApp.accounts.forms import UserRegisterForm, UserEditForm
+from UniversityApp.accounts.models import Profile
 
 UserModel = get_user_model()
 
 
+class ShowProfile(admin.StackedInline):     # allows PROFILE data (all or selected fields)
+    model = Profile                         # to be showcased in admin panel
+    can_delete = False
+    fields = (                  # select which fields to be shown
+        # 'first_name',
+        # 'last_name',
+        # 'personal_image',
+        # 'bio',
+        # 'age',
+        # 'course',
+        'is_lector',
+    )
+
+
 @admin.register(UserModel)
 class UserAdmin(UserAdmin):
+    inlines = (ShowProfile, )       # declare that PROFILE will be showcased beneath USER data
     model = UserModel
-    add_form = UserRegisterForm
-    form = UserEditForm
+    add_form = UserRegisterForm             # user creation form
+    form = UserEditForm                     # user edit form
     list_display = ('email', 'is_staff', 'is_superuser')
-    search_fields = ('email',)
+    search_fields = ('email',)          # search by
     ordering = ('pk',)
 
-    # categories in admin panel (user information):
+    # User information, separated into CATEGORIES (sections):
     fieldsets = (
         ('User email/password', {'fields': ('email', 'password')}),
         ('User info', {'fields': ()}),
@@ -25,11 +41,9 @@ class UserAdmin(UserAdmin):
 
     # when creating new user, which fields to be shown:
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "password1", "password2"),
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
             }
-        ),
+        )
     )
