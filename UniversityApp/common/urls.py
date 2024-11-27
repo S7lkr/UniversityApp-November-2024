7f5f69323a56f04d556dcbf99d164642579ba3c8
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-28 01:23:06 +0200

    FIx: add/remove user from course

--- UniversityApp/common/urls.py
+++ UniversityApp/common/urls.py
@@ -4,5 +4,5 @@ from UniversityApp.common.views import HomePage, course_user_add, course_user_re
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('user-add/<slug:course_slug>/', course_user_add, name="user-add"),
-    path('user-remove/', course_user_remove, name='user-remove'),
+    path('user-remove/<slug:course_slug>', course_user_remove, name='user-remove'),
 ]
--- UniversityApp/common/views.py
+++ UniversityApp/common/views.py
@@ -8,17 +8,24 @@ class HomePage(generic.TemplateView):
 
 
 def course_user_add(request, course_slug):
-    course_id = Course.objects.get(slug=course_slug).pk
+    course = Course.objects.get(slug=course_slug)
     profile = request.user.profile
-    profile.course_id = course_id
+    profile.course_id = course.pk
+    if profile.is_lector:
+        course.lector = profile.get_full_name()
+    course.save()
     profile.save()
 
     return redirect(to='http://localhost:8000/courses/all-courses/')
 
 
-def course_user_remove(request):
+def course_user_remove(request, course_slug):
+    course = Course.objects.get(slug=course_slug)
     profile = request.user.profile
     profile.course_id = None
+    if profile.is_lector:
+        course.lector = None
+    course.save()
     profile.save()
 
     return redirect(to='http://localhost:8000/courses/all-courses/')
