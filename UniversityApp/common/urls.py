76c10b7e67444cea2fe64762ed1c0c63c25fb32b
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-29 00:01:45 +0200

    Update: admin can edit/join/delete course

--- UniversityApp/common/urls.py
+++ UniversityApp/common/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from UniversityApp.common.views import HomePage, course_user_add, course_user_remove
+from UniversityApp.common.views import HomePage, course_user_add, course_user_remove, lector_remove
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('user-add/<slug:course_slug>/', course_user_add, name="user-add"),
     path('user-remove/<slug:course_slug>', course_user_remove, name='user-remove'),
+    path('lector-remove/<slug:course_slug>/', lector_remove, name='lector-remove'),
 ]
