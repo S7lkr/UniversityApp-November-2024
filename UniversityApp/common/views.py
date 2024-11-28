8a815904dc312cbd82686b6fe888dfcdd9d52c7e
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-28 23:59:17 +0200

    Feat: admin can remove lector from course

--- UniversityApp/common/views.py
+++ UniversityApp/common/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import generic
+
+from UniversityApp.accounts.models import Profile
 from UniversityApp.courses.models import Course
 
 
@@ -15,8 +17,8 @@ def course_user_add(request, course_slug):
         course.lector = profile.get_full_name()
     course.save()
     profile.save()
-
-    return redirect(to='http://localhost:8000/courses/all-courses/')
+    return redirect(to=request.META.get('HTTP_REFERER'))
+    # return redirect(to='http://localhost:8000/courses/all/')
 
 
 def course_user_remove(request, course_slug):
@@ -27,6 +29,19 @@ def course_user_remove(request, course_slug):
         course.lector = None
     course.save()
     profile.save()
+    return redirect(to=request.META.get('HTTP_REFERER'))
+    # return redirect(to='http://localhost:8000/courses/all/')
 
-    return redirect(to='http://localhost:8000/courses/all-courses/')
 
+def lector_remove(request, course_slug):
+    course = Course.objects.get(slug=course_slug)
+    l_first_name = course.lector.split(" ")[0]
+    l_last_name = course.lector.split(" ")[1]
+    profile = Profile.objects.get(first_name=l_first_name, last_name=l_last_name)
+    profile.course_id = None
+    course.lector = None
+    course.save()
+    profile.save()
+
+    return redirect(to=request.META.get('HTTP_REFERER'))       # stays on the same path
+    # return redirect(to='http://localhost:8000/courses/all/')
