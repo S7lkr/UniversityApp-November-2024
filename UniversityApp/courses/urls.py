a8d95ab17ded56524440ac6ab50985e1e6bc4a57
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-28 23:56:54 +0200

    Fix: restructure courses urls

--- UniversityApp/courses/urls.py
+++ UniversityApp/courses/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from UniversityApp.courses import views
 
 urlpatterns = [
-    path('all-courses/', views.CoursesAllPage.as_view(), name='courses-all'),
+    path('all/', views.CoursesAllPage.as_view(), name='courses-all'),
+    path('create/', views.CourseCreatePage.as_view(), name='course-create'),
     path('categories/', include([
         path('', views.CoursesCategoriesPage.as_view(), name='categories'),
         path('Web-Design/', include([
             path('', views.CoursesFromCategoryPage.as_view(), name='courses-wd'),
-            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
             path('<slug:course_slug>/', include([
                 path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
                 path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
@@ -16,7 +16,6 @@ urlpatterns = [
         ])),
         path('Graphic-Design/', include([
             path('', views.CoursesFromCategoryPage.as_view(), name='courses-gd'),
-            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
             path('<slug:course_slug>/', include([
                 path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
                 path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
@@ -25,7 +24,6 @@ urlpatterns = [
         ])),
         path('Video-Editing/', include([
             path('', views.CoursesFromCategoryPage.as_view(), name='courses-ve'),
-            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
             path('<slug:course_slug>/', include([
                 path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
                 path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
@@ -34,7 +32,6 @@ urlpatterns = [
         ])),
         path('Online-Marketing/', include([
             path('', views.CoursesFromCategoryPage.as_view(), name='courses-om'),
-            path('create/', views.CourseCreatePage.as_view(), name='courses-create'),
             path('<slug:course_slug>/', include([
                 path('details/', views.CourseDetailsPage.as_view(), name='course-details'),
                 path('edit/', views.CourseEditPage.as_view(), name='course-edit'),
@@ -43,3 +40,4 @@ urlpatterns = [
         ])),
     ])),
 ]
+
