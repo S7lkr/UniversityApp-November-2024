070bffc40a129ea11f4b027f22cf4853bf0857b1
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-30 01:56:30 +0200

    Edit: clear all course edit form labels

--- UniversityApp/courses/forms.py
+++ UniversityApp/courses/forms.py
@@ -3,26 +3,34 @@ from UniversityApp.courses.models import Course
 from UniversityApp import mixins
 
 
-class CourseForm(forms.ModelForm):
+class CourseBaseForm(forms.ModelForm):
     class Meta:
         model = Course
         exclude = ('slug', 'lector',)
-
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date'}),
         }
+        labels = {
+            'name': '',
+            'category': '',
+            'description': '',
+            'start_date': '',
+            'credits': '',
+            'duration': '',
+            'photo': '',
+        }
 
 
-class CourseCreateForm(mixins.PlaceholderMixin, CourseForm):
+class CourseCreateForm(mixins.PlaceholderMixin, CourseBaseForm):
     # disabled_fields_set = ('category',)
     pass
 
 
-class CourseEditForm(CourseForm):
+class CourseEditForm(CourseBaseForm):
     pass
 
 
-class CourseDeleteForm(mixins.DisabledFieldsMixin, CourseForm):
+class CourseDeleteForm(mixins.DisabledFieldsMixin):
     # disabled_fields_set = ('name', 'category',)
 
     class Meta:
