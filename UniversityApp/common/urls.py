0f2dc42dc037595f2710b76321f86429c212dac1
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-11-30 01:56:50 +0200

    Fix: reworked common urls

--- UniversityApp/common/urls.py
+++ UniversityApp/common/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from UniversityApp.common.views import HomePage, course_user_add, course_user_remove, lector_remove, comment_add_view
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
-    path('user-add/<slug:course_slug>/', course_user_add, name="user-add"),
-    path('user-remove/<slug:course_slug>', course_user_remove, name='user-remove'),
-    path('lector-remove/<slug:course_slug>/', lector_remove, name='lector-remove'),
-    path('add-comment/<slug:course_slug>/', comment_add_view, name='add-comment'),
+    path('courses/categories/<slug:category_slug>/', include([
+        path('<int:course_pk>/details/', include([
+            path('user-add/', course_user_add, name="user-add"),
+            path('user-remove/', course_user_remove, name='user-remove'),
+            path('lector-remove/', lector_remove, name='lector-remove'),
+            path('add-comment/', comment_add_view, name='add-comment'),
+        ])),
+    ])),
 ]
