b8dcd2738a5511c30ca1abf855922d1e91951228
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-12-06 12:56:08 +0200

    Add: perm. check filter & err handling

--- UniversityApp/common/templatetags/custom_filters.py
+++ UniversityApp/common/templatetags/custom_filters.py
@@ -1,25 +1,48 @@
 from django import template
+from django.contrib.auth import get_user_model
 
 register = template.Library()
 
 
 @register.filter(name='key')
 def key(dict_, key_):
-    if dict_.__class__ == dict:
+    """
+    Custom filter which takes 2 args: "dict" and "key".
+    Checks if first arg is NOT a dict: raises ValueError
+    """
+    if dict_.__class__ != dict:
+        raise ValueError('Error: The first given argument "dict_" must be of class "dict"!')
+    try:
         if type(key_) is str:
             return dict_[key_]
         elif type(key_) is int:
             return dict_.key_
-    raise ValueError('The given argument "dict_" must be of class "dict"!')
+    except KeyError as err:
+        print(err)
 
 
 @register.filter(name='index')
 def index(iterable, index_: int):
-    if iterable.__class__ in [list, set, tuple]:
+    if iterable.__class__ not in [list, tuple]:
+        raise ValueError('The given argument "iterable" must be of class "list" or "tuple"!')
+    try:
         return iterable[index_ - 1]
-    raise ValueError('The given argument "iterable" must be of class "list/set/tuple"!')
+    except IndexError as err:
+        print(err)
 
 
 @register.filter(name='as_list')
 def str_to_list(value: str):
-    return eval(value)
+    if value.__class__ != str:
+        raise ValueError('The given argument must be of type String!')
+    try:
+        return eval(value)
+    except TypeError as err:
+        print(err)
+
+
+@register.filter(name='allowed_to')
+def allowed_to(user, action: str):
+    if user.__class__ != get_user_model().__class__:
+        raise TypeError('Given argument "user" must be of class "CustomUser"!')
+    return action in user.get_group_permissions() or user.has_perm(action)
